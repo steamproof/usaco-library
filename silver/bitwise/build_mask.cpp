f04fb9d4df1c68b510f0c1cb415a014f97b41ead
steamproof <93405617+steamproof@users.noreply.github.com>
2026-01-09 17:09:38 -0800

    Update build_mask.cpp

--- silver/bitwise/build_mask.cpp
+++ silver/bitwise/build_mask.cpp
@@ -1,6 +1,5 @@
 template <typename T>
 T build_mask(const string& s, char target) {
-    static_assert(is_integral_v<T>, "T must be an integral type");
     T mask = 0;
     for (int i = 0; i < (int)s.size(); i++) {
         if (s[i] == target) {
